87e45afba1a15da009ab259d6f202aa9843c5abd
Gabrielle Ribeiro <gabrielleribeiro2010@gmail.com>
2022-11-19 22:19:58 -0300

    Add problem 1610

---
+++ 1610 - Dudu faz serviço/1610.py
@@ -0,0 +1,82 @@
+'''
+This problem detects with there is a cicle in a graph.
+We use DFS and represent our graph with an adjacency 
+matrix. In this problem we have a directed graph.
+'''
+
+# Guarantees that the online judge's recursion stack will not overflow
+import sys
+sys.setrecursionlimit(1048576)
+
+# Adjacency matrix
+dependencies = []
+
+# Visited vector
+visited_dependencies = []
+
+has_cicle = False
+
+
+def isVisited(vertex):
+    '''
+    Check if a vertex is already visited
+    '''
+    if visited_dependencies[vertex] == 1:
+        return True
+    return False
+
+    
+def DFS(vertex):
+    '''
+    An implementation of Depth First Search for graphs
+    with a small change to allow detection of cycles
+    '''
+
+    visited_dependencies[vertex] = 1
+
+    global has_cicle
+
+    for i in range(len(dependencies[vertex])):
+        document = dependencies[vertex][i]
+        if visited_dependencies[document] == 1:
+            has_cicle = True
+            return
+        elif visited_dependencies[document] == 0:
+            DFS(document)
+
+    visited_dependencies[vertex] = 2
+
+
+test_cases = int(input())
+
+for i in range(test_cases):
+    line = input().split()
+
+    amount_of_documents = int(line[0])
+    amount_of_dependecies = int(line[1])
+
+    dependencies.clear()
+    visited_dependencies.clear()
+    has_cicle = False
+
+    # Initialize adjacency matrix and visited vector
+    for _ in range(amount_of_documents):
+        vertex_list = []
+        dependencies.append(vertex_list)
+        visited_dependencies.append(0)
+
+    for _ in range(amount_of_dependecies):
+        line = input().split()
+        document_a = int(line[0])
+        document_b = int(line[1])
+
+        dependencies[document_a - 1].append(document_b - 1)
+
+    for vertex in range(amount_of_documents):
+        if not isVisited(vertex):
+            DFS(vertex)
+        
+        if has_cicle:
+            break
+
+    print('SIM' if has_cicle else 'NAO')
---
+++ 1610 - Dudu faz serviço/README.md
@@ -0,0 +1,30 @@
+# Dudu Faz Serviço
+
+*Questão 1610 | Beecrowd*  
+*[Link para o problema](https://www.beecrowd.com.br/repository/UOJ_1610.html)*
+
+### **Descrição**
+
+Dudu precisa de um documento para finalizar uma tarefa em seu trabalho. Após pesquisar um pouco, ele descobre que este documento depende de outros documentos que, por sua vez, necessitam de outros documentos e assim por diante.
+
+Dudu chegou a uma lista final com todos os documentos que deverá precisar. Com essa lista em mãos, ele suspeita que a mesma possui loops. Por exemplo, se um documento A depende do documento B que por sua vez depende do documento A, tornaria a tarefa interminável. Veja que neste caso o loop tem apenas dois documentos, pode haver loops com três ou mais!
+
+Dada a lista das dependências entre os documentos, ajude Dudu a saber se um dia conseguirá todos os documentos, ou seja, se não existe um loop na lista.
+
+### **Entrada**
+
+Na primeira linha você terá um inteiro **T** (**T** = 100) indicando o número de casos de teste.
+
+Na primeira linha de cada caso teremos os números inteiros **N** (2 ≤ **N** ≤ 100* ou 2 ≤ **N** ≤ 104**) e **M** (1 ≤ **M** ≤ 300* ou 1 ≤ **M** ≤ 3*104​**), indicando o número de documentos e as dependências existentes. Em cada uma das **M** linhas seguintes, terão dois inteiros **A** (1 ≤ **A**) e **B** (**B** ≤ **N**, com **A** != **B**), indicando que o documento A depende do documento **B**. Pode haver dependências repetidas!
+
+*Ocorre em aproximadamente 90% dos casos de teste;
+
+**Ocorre nos demais casos de teste.
+
+### **Saída**
+
+Para cada caso, imprima SIM caso exista pelo menos um loop e NAO caso contrário.
+
+| **Exemplo de Entrada** | **Exemplo de Saída**|
+|-------|--------|
+|3<br>2 1<br>1 2<br>2 2<br>1 2<br>2 1<br>4 4<br>2 3<br>3 4<br>4 2<br>1 3|NAO<br>SIM<br>SIM
